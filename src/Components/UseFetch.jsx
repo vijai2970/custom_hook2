3a1e4d7faa1a371ef6f304020efe4f3862d818bf
VS <vijai2970@gmail.com>
2024-07-21 16:37:21 -0400

    first lab change

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,11 @@
+import React, {useState, useEffect } from "react";
 const UseFetch = (url) => {
-    
+     const[data,setData]=useState();
+      useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+return [data]
 }
 
 export default UseFetch
